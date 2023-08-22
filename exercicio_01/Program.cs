8c73241e43561a00dd0c07b516c52fe5801abefa
Darukkyh <de.coding95@gmail.com>
2023-08-22 12:13:50 -0300

    resolution

---
+++ exercicio_01/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program {
+    static void Main() {
+        int a = 10;
+        Console.WriteLine("a = " + a);
+        int b = 20;
+        Console.WriteLine("b = " + b);
+        int c = (a + b) / 2;
+        Console.WriteLine("c = " + c);
+        c -=40;
+        Console.WriteLine("c = " + c);
+        int[] v = new int[4];
+        v[3] = a+b+c;
+
+        Console.WriteLine("v[3] = " + v[3]);        
+    }
+}
