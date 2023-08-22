222505181a21b0b1095d7294998a474a4be0d166
Darukkyh <de.coding95@gmail.com>
2023-08-22 14:47:19 -0300

    resolution

---
+++ Desafio_01/exercicio_02/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program {
+    static void Main() {
+        int a = 2;
+        Console.WriteLine("a = " + a);
+        int[] v = new int[6];
+        while (a < 6) {
+            v[a] = 10 * a;
+            Console.WriteLine($"v[{a}] = " + v[a]);
+            a+=1;
+            Console.WriteLine("a = " + a);
+        }
+    }
+}
